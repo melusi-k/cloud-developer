08830e6cbf350372dd0fab85dfa5ae6738139a9a
Melusi Khumalo <melusi.l.k@gmail.com>
2022-12-20 17:43:19 +0200

    Added switch statement to test

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,15 +1,15 @@
-import express, { Router, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Router, Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { reject } from "bluebird";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -29,37 +29,56 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  try {
-  app.get("/filteredimage", async(req: Request, res: Response)=>{
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    let image_url: any = req.query.image_url;
+    switch (image_url) {
+      case (!image_url):
+        console.log("No url");
+        break;
+      case (image_url.endsWith(".jpg")):
+        console.log("Good Image");
+        break;
+    
+      default:
+        console.log("Hit defualt");
+        
+        break;
+    }
 
-     let image_url = req.query.image_url;
-     if(!image_url){
-       res.status(422).send("Enter a valid image URL")
-       console.log(image_url);
-     }else{
-     let filteredimage = await filterImageFromURL(image_url);
 
-     res.status(200).sendFile(filteredimage, () =>{
-       deleteLocalFiles([filteredimage])
-     }) 
-     }
-   })
-  } catch(error) {
-    console.log(`Error: ${error}`);
-  }
-  
+
+    if (!image_url) {
+      res.status(422).send("Enter a valid image URL");
+    }
+    if (image_url.endsWith(".jpg")) {
+      console.log("Good Image");
+      let filteredimage = await filterImageFromURL(image_url);
+
+      res.status(200).sendFile(filteredimage, () => {
+        deleteLocalFiles([filteredimage]);
+      });
+    } else {
+      console.log("Bad man");
+      res
+        .status(422)
+        .send(
+          "You have entered an unprocessable image, Please enter a valid image url."
+        );
+    }
+    
+  });
+
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
